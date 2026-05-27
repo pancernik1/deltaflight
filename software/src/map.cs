3283aeb83ad5bc20ca85f2b1ade8bf4d40ed4ff4
Pancernik <pancernik.bis@gmail.com>
2026-05-27 22:19:19 +0200

    Scaling fix
    I fixed  all of the scaling issues and added some debugging.
    
    This commit took ~50  minutes.

--- software/src/map.cs
+++ software/src/map.cs
@@ -4,13 +4,13 @@ using System.IO;
 using SkiaSharp;
 using MaxRev.Gdal.Core;
 using OSGeo.GDAL;
-using System.Linq;
+
 public class map
 {
-    public int offx =0;
-    public int offy =0;
+    public int offx = 0;
+    public int offy = 0;
 
-    static public (string? argName,int? argVal) parseArg(string arg)
+    static public (string? argName, int? argVal) parseArg(string arg)
     {
         string? argName = null;
         int? argVal = null;
@@ -20,134 +20,92 @@ public class map
         argList[2] = "scale";
         string extrName = "";
         string extrVal = "";
-        int? eqI =null; // '=' sign index
-        //extract sign
-        for(int i = 0; i<arg.Length;i++)
+        int? eqI = null;
+
+        for (int i = 0; i < arg.Length; i++)
         {
-            if(arg[i]=='=')
-            {
-                eqI = i;
-            }
+            if (arg[i] == '=') eqI = i;
         }
-     if(eqI != null)
-     {
-        for(int i = 0; i<arg.Length;i++)
+
+        if (eqI != null)
         {
-            if(i<eqI)
+            for (int i = 0; i < arg.Length; i++)
             {
-                extrName += arg[i]; 
+                if (i < eqI)      extrName += arg[i];
+                else if (i > eqI) extrVal  += arg[i];
             }
-            else if(i>eqI)
+
+            for (int i = 0; i < argList.Length; i++)
             {
-                extrVal += arg[i];
+                if (extrName == argList[i]) { argName = argList[i]; break; }
+                else argName = null;
             }
-        }        
-        for(int i = 0;i< argList.Length;i++)
-        {
-            if(extrName == argList[i])
-            {
-                argName = argList[i];
-                break;
-            }
-            else
-            {
-                argName = null;
-            }
+
+            try   { argVal = int.Parse(extrVal); }
+            catch { argVal = null; }
+
+            return (argName, argVal);
         }
-        try
-        {
-            argVal = int.Parse(extrVal);
-        }
-        catch
-        {
-            argVal = null;
-        }
-        return (argName,argVal);
-     }
         else
         {
-            return (null,null);
+            return (null, null);
         }
     }
-    static public List<string> getArgs(string cmd,int qtEnd)
+
+    static public List<string> getArgs(string cmd, int qtEnd)
     {
-        string msg = "";
         List<int> dIndexes = new List<int>();
-        List<string> args = new List<string>();
-      for(int i = 0; i < cmd.Length;i++)
-      {
-        if(cmd[i] == '-')
+        List<string> args  = new List<string>();
+
+        for (int i = 0; i < cmd.Length; i++)
         {
-            dIndexes.Add(i);
+            if (cmd[i] == '-') dIndexes.Add(i);
         }
-      }
-      for(int i = 0; i < dIndexes.Count;i++)
-      {
-        args.Add("");
-        for(int ch = 1; dIndexes[i]+ch < cmd.Length && cmd[dIndexes[i]+ch]!=' ';ch++)
+
+        for (int i = 0; i < dIndexes.Count; i++)
         {
-            args[i] += cmd[dIndexes[i]+ch];
+            args.Add("");
+            for (int ch = 1; dIndexes[i] + ch < cmd.Length && cmd[dIndexes[i] + ch] != ' '; ch++)
+                args[i] += cmd[dIndexes[i] + ch];
         }
-      }
-      for(int i = 0; i< args.Count;i++)
-      {
-        msg += args[i] + ",";
-      }
+
         return args;
     }
 
-   public static string checkForExceptions(List<int> arr, string _path,
-    string ext,string cmd)
+    public static string checkForExceptions(List<int> arr, string _path, string ext, string cmd)
     {
-        
-        if (arr.Count % 2 != 0)
-            return "Quotes aren't closed";
-        else if (arr.Count < 2)
-            return "Add path to the image you want to convert";
-        else if (arr.Count > 2)
-            return "Multiple paths not supported";
-        else if (arr[1] - arr[0] < 2)
-            return "No path found";
-        else if (!File.Exists(_path))
-            return "File doesn't exist";
-        else if (ext != "jp2")
-            return "Wrong extension";
-        
-        List<string> args = getArgs(cmd,arr[1]);
+        if (arr.Count % 2 != 0)      return "Quotes aren't closed";
+        if (arr.Count < 2)           return "Add path to the image you want to convert";
+        if (arr.Count > 2)           return "Multiple paths not supported";
+        if (arr[1] - arr[0] < 2)     return "No path found";
+        if (!File.Exists(_path))     return "File doesn't exist";
+        if (ext != "jp2")            return "Wrong extension";
+
+        List<string>  args     = getArgs(cmd, arr[1]);
         List<string?> argNames = new List<string?>();
-        List<int?> argVals = new List<int?>();
-        if(args.Count > 0)
+        List<int?>    argVals  = new List<int?>();
+
+        if (args.Count > 0)
         {
-            for(int i =0; i<args.Count;i++)
+            for (int i = 0; i < args.Count; i++)
             {
                 argNames.Add(parseArg(args[i]).argName);
                 argVals.Add(parseArg(args[i]).argVal);
             }
-            if (argNames.Contains(null))
-            {
-                return "unknown argument found"; 
-            }
-            else if(argVals.Contains(null))
-            {
-                return "Couldn't parse argument value (it has to be int)";
-            }
-            else
-            {
-                return null;
-            }
+            if (argNames.Contains(null)) return "unknown argument found";
+            if (argVals.Contains(null))  return "Couldn't parse argument value (it has to be int)";
         }
+
         return null;
-        
     }
 
     public static string path(string input)
     {
         List<int> quoteIndexes = new List<int>();
-        string indexes = "";
-        string path = "";
-        string ext = "";
+        string path  = "";
+        string ext   = "";
         int dotIndex = 0;
-        int scale =1;
+        int scale    = 1;
 
         for (int i = 0; i < input.Length; i++)
         {
@@ -164,59 +122,44 @@ public class map
             }
         }
 
-        foreach (int n in quoteIndexes)
-            indexes += n.ToString() + ',';
-
-
         try
         {
-        if (checkForExceptions(quoteIndexes, path, ext, input) == null)
-        {   
-            int offX =0;
-            int offY =0;
-            
-            List<string> args = getArgs(input,quoteIndexes[1]);
-            List<string?> argNames = new List<string?>();
-            List<int?> argVals = new List<int?>();
-            if(args.Count > 0)
+            if (checkForExceptions(quoteIndexes, path, ext, input) == null)
             {
-              for(int i =0; i<args.Count;i++)
+                int offX = 0;
+                int offY = 0;
+
+                List<string> args = getArgs(input, quoteIndexes[1]);
+
+                if (args.Count > 0)
                 {
-                 if(parseArg(args[i]).argName == "offX")
-                 {
-                    offX = parseArg(args[i]).argVal ?? 0;
-                 }
-                 
-                 if(parseArg(args[i]).argName == "offY")
-                 {
-                    offY = parseArg(args[i]).argVal ?? 0;
-                 }
-                 if(parseArg(args[i]).argName == "scale")
-                 {
-                    if(parseArg(args[i]).argVal != 0)
+                    for (int i = 0; i < args.Count; i++)
                     {
-                    scale = parseArg(args[i]).argVal ?? 1;
+                        if (parseArg(args[i]).argName == "offX")
+                            offX = parseArg(args[i]).argVal ?? 0;
+                        if (parseArg(args[i]).argName == "offY")
+                            offY = parseArg(args[i]).argVal ?? 0;
+                        if (parseArg(args[i]).argName == "scale" && parseArg(args[i]).argVal != 0)
+                            scale = parseArg(args[i]).argVal ?? 1;
                     }
-                 }
-                 
                 }
 
+                jp2ds(path, offX, offY, scale);
             }
-            jp2ds(path,offX,offY,scale);
-            
+            else
+            {
+                return "Path error- " + checkForExceptions(quoteIndexes, path, ext, input);
+            }
         }
-        else
-        {
-            return "Path error- " + checkForExceptions(quoteIndexes, path, ext, input);
-        }
-        }
-        catch(System.Exception err)
+        catch (System.Exception err)
         {
             return err.ToString();
         }
-        return scale.ToString();
+
+        return "conversion successful, scale=" + scale;
     }
-static public void jp2ds(string _path, int offX, int offY, int scale)
+
+    static public void jp2ds(string _path, int offX, int offY, int scale)
 {
     GdalBase.ConfigureAll();
     Gdal.AllRegister();
@@ -226,15 +169,16 @@ static public void jp2ds(string _path, int offX, int offY, int scale)
     int fullWidth  = ds.RasterXSize;
     int fullHeight = ds.RasterYSize;
 
-    int outWidth   = 600 * scale;
-    int outHeight  = 400 * scale;
+    int outWidth  = 600;
+    int outHeight = 400;
     int pixelCount = outWidth * outHeight;
 
     int srcOffX = Math.Clamp(offX, 0, fullWidth  - 1);
     int srcOffY = Math.Clamp(offY, 0, fullHeight - 1);
 
-    int srcRegionW = Math.Clamp(fullWidth  - srcOffX, 1, fullWidth);
-    int srcRegionH = Math.Clamp(fullHeight - srcOffY, 1, fullHeight);
+    // scale=1 reads full image, scale=2 reads half, etc.
+    int srcRegionW = Math.Clamp(outWidth  * scale, 1, fullWidth  - srcOffX);
+    int srcRegionH = Math.Clamp(outHeight * scale, 1, fullHeight - srcOffY);
 
     Band bandR = ds.GetRasterBand(1);
     Band bandG = ds.GetRasterBand(2);
@@ -244,15 +188,16 @@ static public void jp2ds(string _path, int offX, int offY, int scale)
     byte[] bufG = new byte[pixelCount];
     byte[] bufB = new byte[pixelCount];
 
-    bandR.ReadRaster(srcOffX, srcOffY, srcRegionW , srcRegionH , bufR, outWidth, outHeight, 0, 0);
-    bandG.ReadRaster(srcOffX, srcOffY, srcRegionW , srcRegionH , bufG, outWidth, outHeight, 0, 0);
-    bandB.ReadRaster(srcOffX, srcOffY, srcRegionW , srcRegionH , bufB, outWidth, outHeight, 0, 0);
+    bandR.ReadRaster(srcOffX, srcOffY, srcRegionW, srcRegionH, bufR, outWidth, outHeight, 0, 0);
+    bandG.ReadRaster(srcOffX, srcOffY, srcRegionW, srcRegionH, bufG, outWidth, outHeight, 0, 0);
+    bandB.ReadRaster(srcOffX, srcOffY, srcRegionW, srcRegionH, bufB, outWidth, outHeight, 0, 0);
 
-    var bmp = new SKBitmap(outWidth / scale, outHeight /scale, SKColorType.Rgb888x, SKAlphaType.Opaque);
-    for (int y = 0; y < outHeight; Math.Min(y += scale,outHeight))
-        for (int x = 0; x < outWidth; Math.Min(x += scale,outWidth))
+    var bmp = new SKBitmap(outWidth, outHeight, SKColorType.Rgb888x, SKAlphaType.Opaque);
+    for (int y = 0; y < outHeight; y++)
+        for (int x = 0; x < outWidth; x++)
         {
-            int i = y * (outWidth/scale) + x;
+            int i = y * outWidth + x;
+            
             bmp.SetPixel(x, y, new SKColor(bufR[i], bufG[i], bufB[i]));
         }
 
@@ -262,5 +207,15 @@ static public void jp2ds(string _path, int offX, int offY, int scale)
     encoded.SaveTo(stream);
 
     ds.Dispose();
+    //Debug
+    Console.WriteLine("Image created");
+    Console.WriteLine("srcOffX - " + srcOffX.ToString());
+    Console.WriteLine("srcOffY - " + srcOffY.ToString());
+    Console.WriteLine("outWidth - " + outWidth.ToString());
+    Console.WriteLine("outHeight - " + outHeight.ToString());
+    Console.WriteLine("srcRegionW - " + srcRegionW.ToString());
+    Console.WriteLine("srcRegionH - " + srcRegionH.ToString());
+    Console.WriteLine("fullWidth - " + fullWidth.ToString());
+    Console.WriteLine("fullHeight - " + fullHeight.ToString());
 }
 }
